a2c2abc5ad174be5ddd3072b78575a34153b540f
CZSM4 <poojithanalluri80@gmail.com>
2018-01-20 18:56:27 +0530

    dont merge

--- app/src/main/java/io/github/froger/instamaterial/Utils.java
+++ app/src/main/java/io/github/froger/instamaterial/Utils.java
@@ -1,11 +1,14 @@
 package io.github.froger.instamaterial;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Point;
 import android.os.Build;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 /**
  * Created by froger_mcs on 05.11.14.
@@ -44,5 +47,15 @@ public class Utils {
 
     public static boolean isAndroid5() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
