7da9970a0513ff781c68de20579f38e926218229
Adavan Ayyappan <adavan@czsm.co.in>
2018-01-25 17:19:59 +0530

    Login prefrences issue

--- app/src/main/java/czsm/github/froger/instamaterial/ui/activity/LoginScreen.java
+++ app/src/main/java/czsm/github/froger/instamaterial/ui/activity/LoginScreen.java
@@ -8,8 +8,10 @@ import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -21,13 +23,21 @@ import android.widget.Toast;
 
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import czsm.github.froger.instamaterial.R;
 import czsm.github.froger.instamaterial.Utils;
+import czsm.github.froger.instamaterial.ui.Models.Post;
+import czsm.github.froger.instamaterial.ui.Models.Users;
+import czsm.github.froger.instamaterial.ui.utils.PreferencesHelper;
 
 public class LoginScreen extends AppCompatActivity {
     EditText mEmailEdt,mPassEdt;
@@ -76,9 +86,11 @@ public class LoginScreen extends AppCompatActivity {
         mAccntTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginScreen.this, SignupActivity.class));
+                Intent intent = new Intent(getApplicationContext(),SignupActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
                 overridePendingTransition(R.anim.slide_up, R.anim.stay);
-                finish();
+
             }
         });
 
@@ -109,10 +121,44 @@ public class LoginScreen extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Intent in=new Intent(LoginScreen.this,MainActivity.class);
-                            startActivity(in);
-                            // Sign in success, update UI with the signed-in user's information
 
+                            final FirebaseUser users = mAuth.getCurrentUser();
+                            FirebaseFirestore db = com.google.firebase.firestore.FirebaseFirestore.getInstance();
+                            DocumentReference docRef = db.collection("users").document(users.getUid());
+                            docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                @Override
+                                public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+
+                                    if (documentSnapshot.exists()){
+
+                                        Users user = documentSnapshot.toObject(Users.class);
+
+
+
+                                        PreferencesHelper.setPreference(getApplicationContext(), PreferencesHelper.PREFERENCE_EMAIL,user.getuserName());
+                                        PreferencesHelper.setPreferenceBoolean(getApplicationContext(), PreferencesHelper.PREFERENCE_LOGGED_IN,true);
+
+                                        PreferencesHelper.setPreference(getApplicationContext(), PreferencesHelper.PREFERENCE_PROFILE_PIC, user.getProfileImageURL());
+                                        PreferencesHelper.setPreference(getApplicationContext(), PreferencesHelper.PREFERENCE_FIREBASE_UUID, users.getUid());
+
+                                        Intent in=new Intent(LoginScreen.this,MainActivity.class);
+                                        startActivity(in);
+
+                                    } else {
+                                        Toast.makeText(LoginScreen.this, "No user exits", Toast.LENGTH_LONG).show();
+
+                                    }
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+
+                                    Log.w("Error", "Error adding document", e);
+                                    Toast.makeText(getApplicationContext(),"Login failed",Toast.LENGTH_SHORT).show();
+                                }
+                            });
 
                         } else {
                             // If sign in fails, display a message to the user.
